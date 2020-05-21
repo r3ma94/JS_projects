c55eafdfcfd7eadbeaa713fc904abfcf2d3c23c5
r3ma94 <r3ma94@gmail.com>
2020-05-22 00:29:40 +0200

    commit

--- ZanimljivaGeografijaApp/app.js
+++ ZanimljivaGeografijaApp/app.js
@@ -10,31 +10,71 @@ let selection = document.querySelector("#select");
 let pojamInput = document.querySelector("#inputPojam");
 let pojamButton = document.querySelector("#buttonPojam");
 
+let selectionDiv = document.querySelector("#selection");
 
-//events
+
+let regEx = /[!@#$%^&*(),.?":{}|<>0-9_\s]/g;
+
+
 userNameForm.addEventListener("submit", () =>{
     localStorage.setItem("userNameLS", userName.value);
 });
 
-pojamButton.addEventListener("click", ()=>{
+if(localStorage.userNameLS === undefined) {
+    selectionDiv.classList.add("invisible");
+}
+else {
+    selectionDiv.classList.remove("invisible");
+}
+
+
+
+//events
+pojamButton.addEventListener("click", event => {
+    event.preventDefault();
+
+    let pocetnoSlovo = capitalizeFirstLetter(pojamInput.value.charAt(0));
+    let noviPojam = capitalizeFirstLetter(pojamInput.value.replace(regEx, ''));
+
+
     let date = new Date();
-    db.collection("pojmovi").doc().set({
-        pocetno_slovo: capitalizeFirstLetter(pojamInput.value.charAt(0)),
-        kategorija: selection.value,
-        pojam: capitalizeFirstLetter(pojamInput.value.replace(/[^a-zA-Z]/g, '')),
-        korisnik: localStorage.getItem("userNameLS"),
-        vreme: firebase.firestore.Timestamp.fromDate(date)
-        
+    let provera = false;
+
+
+    db.collection("pojmovi")
+    .where('kategorija', '==', selection.value)
+    .where('pojam', '==', noviPojam)
+    .get()
+    .then(snapshot => {
+        snapshot.docs.forEach(doc => {
+            if(noviPojam == doc.data().pojam) {
+                provera = true;
+                
+            }
+        });
+
+        if(provera) {
+            alert('Pojam postoji')
+        }
+        else {
+        db.collection("pojmovi").doc().set({
+            pocetno_slovo: pocetnoSlovo,
+            kategorija: selection.value,
+            pojam: noviPojam,
+            korisnik: localStorage.getItem("userNameLS"),
+            vreme: firebase.firestore.Timestamp.fromDate(date)
+            
+        })
+        .then(function(){
+            console.log("Pojam uspesno dodat!")
+        })
+        .catch(function(error){
+            console.error("Nije uspelo dodavanje pojma: ", error)
+        });
+        }
     })
-    .then(function(){
-        console.log("Pojam uspesno dodat!")
-    })
-    .catch(function(error){
-        console.error("Nije uspelo dodavanje pojma: ", error)
-    });
 
 });
-
 
 
 
