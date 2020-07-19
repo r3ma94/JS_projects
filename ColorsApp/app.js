3c7d6ee81e0010dc2d7d1d3dc8933f034fdabaaf
r3ma94 <r3ma94@gmail.com>
2020-07-19 19:40:56 +0200

    Addes color to sliders

--- ColorsApp/app.js
+++ ColorsApp/app.js
@@ -22,7 +22,50 @@ function randomColors(){
         // Add color to background
         div.style.backgroundColor = randomColor;
         hexText.innerText = randomColor;
+        // check for contrast 
+        checkTextContrast(randomColor, hexText);
+        // Initial colorize sliders
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll(".sliders input");
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+        colorizeSliders(color, hue, brightness, saturation)
+        
     });
 }
+
+function checkTextContrast(color, text){
+    const luminance = chroma(color).luminance();
+    if(luminance > 0.5){
+        text.style.color = "black";
+    }
+    else{
+        text.style.color = "white";
+    } 
+}
+
+function colorizeSliders(color, hue, brightness, saturation){
+    // scale saturation
+    const noSat = color.set("hsl.s", 0);
+    const fullSat = color.set("hsl.s", 1);
+    const scaleSat = chroma.scale([noSat, color, fullSat]);
+    // scale brightness
+    const midBright = color.set("hsl.l", 0.5);
+    const scaleBright = chroma.scale(["black", midBright, "white"]);
+    
+
+    // Update input colors
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)})`;
+
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(0)},${scaleBright(0.5)}, ${scaleBright(1)})`;
+
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(255, 0, 0), rgb(255,255 ,0),rgb(0, 255, 0),rgb(0, 255, 255),rgb(0,0,255),rgb(255,0,255),rgb(255,0,0))`;
+};
+
+
+
+
 
 randomColors();
